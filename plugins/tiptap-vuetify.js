2801c154ea4fe6be1f401cac453378dea15ea9e8
Amanuel Mekonnen <amanuel.mekonnen@auto1.com>
2019-10-30 18:21:30 +0100

    temp 2

--- plugins/tiptap-vuetify.js
+++ plugins/tiptap-vuetify.js
@@ -2,18 +2,9 @@ import Vue from 'vue'
 import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
 import 'tiptap-vuetify/dist/main.css'
 
-import VuetifyDialog from 'vuetify-dialog'
-import 'vuetify-dialog/dist/vuetify-dialog.css'
-
 export default ({ app }) => {
     Vue.use(TiptapVuetifyPlugin, {
         vuetify: app.vuetify,
         iconsGroup: 'md'
     })
-
-//    Vue.use(VuetifyDialog, {
-//        context: {
-//            vuetify: app.vuetify
-//        }
-//    })
 }
